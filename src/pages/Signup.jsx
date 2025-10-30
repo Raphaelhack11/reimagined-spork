3c08817958126522259a6ad9c77040a514fdcd7a
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-30 09:14:12 -0400

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate, link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import Toast from "../components/Toast"; // Import the new Toast component
 
 // Helper function to calculate password strength
